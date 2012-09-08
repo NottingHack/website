103187bb444ce60932f4929b7c74e7705be9e758
James Hayward <jhayward1980@gmail.com>
2012-09-08 21:18:39 +0100

    Changed deploy script to execute in higher directory

--- public_html/deploy.php
+++ public_html/deploy.php
@@ -7,6 +7,8 @@
 * https://gist.github.com/1809044
 */
 
+chdir(..);
+
 // The commands
 $commands = array(
 				  'echo $PWD',
@@ -37,7 +39,7 @@ foreach($commands AS $command){
 	<meta charset="UTF-8">
 	<title>GIT DEPLOYMENT SCRIPT</title>
 </head>
-<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;font-family:mono">
+<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;font-family:monospace">
 <pre>
 . ____ . ____________________________
 |/ \| | |
