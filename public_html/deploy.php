a988db3c5f7fdbad287dd667e20bda92309c7bbf
James Hayward <jhayward1980@gmail.com>
2012-09-08 21:15:53 +0100

    Styling of deploy script

--- public_html/deploy.php
+++ public_html/deploy.php
@@ -37,7 +37,7 @@ foreach($commands AS $command){
 	<meta charset="UTF-8">
 	<title>GIT DEPLOYMENT SCRIPT</title>
 </head>
-<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;font-family:mono">
 <pre>
 . ____ . ____________________________
 |/ \| | |
