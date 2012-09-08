3f584d2b82dd12cbbed88b9f48c647106fefa017
James Hayward <jhayward1980@gmail.com>
2012-09-08 21:29:18 +0100

    Testing again

--- public_html/deploy.php
+++ public_html/deploy.php
@@ -37,7 +37,7 @@ foreach($commands AS $command){
 <html lang="en-US">
 <head>
 	<meta charset="UTF-8">
-	<title>GIT DEPLOYMENT SCRIPT</title>
+	<title>Nottinghack Deployment Script</title>
 </head>
 <body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
 <pre>
