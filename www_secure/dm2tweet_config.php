66a4b343971224f677f87c26269ebc9449b90814
James Hayward <jhayward1980@gmail.com>
2012-09-09 15:54:27 +0100

    Adding dm2tweet config file (but removed oauth keys into seperate file)

---
+++ www_secure/dm2tweet_config.php
@@ -0,0 +1,10 @@
+<?php
+require_once(SECURE_DIR . 'dm2tweet_secure.php');
+
+$aAuthorisedUsers = array(
+						  'geeksareforlife',
+						  'chickengrylls',
+						  'dpslwk',
+						  'nachimir',
+						  );
+?>
