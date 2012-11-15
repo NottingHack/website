59c1817fe0ac93209941004e17cc8e985dd7540d
James Hayward <jhayward1980@gmail.com>
2012-11-15 23:04:04 +0000

    Outputting wiki table

--- public_html/tools/bm/process.php
+++ public_html/tools/bm/process.php
@@ -6,6 +6,13 @@ $aMinutes = file($sFile, FILE_IGNORE_NEW_LINES);
 
 $aIgnoreNames = array("nh-holly");
 
+$aColours = array("00C322", "FF1300", "3E13AF", "FFD700", "CD0074", "FFAA00", "9FEE00", "009999", "A64B00", "D8005F");
+$iColourID = 0;
+$aUserMaps = array();
+
+$sOutput = '{| class="wikitable"' . "\n";
+$sOutput .= "!Time!!Name!!Minute\n";
+
 foreach ($aMinutes as $sMinute) {
 	if (strpos($sMinute, '[') === 0) {
 		list($sTime, $sMinute) = explode("]", $sMinute);
@@ -20,14 +27,32 @@ foreach ($aMinutes as $sMinute) {
 		}
 		
 		if (!in_array($sName, $aIgnoreNames)) {
-			outputMinute($sTime, $sName, $sMinute);
+			$sOutput .= outputMinute($sTime, $sName, $sMinute);
 		}
 	}
 }
 
+$sOutput .= "|}\n";
+
+echo($sOutput);
 
 function outputMinute($sTime, $sName, $sMinute) {
-	echo($sTime . " > " . $sName . " > " . $sMinute . "<br />\n");
+	global $aUserMaps, $aColours, $iColourID;
+	
+	if (!isset($aUserMaps[$sName])) {
+		if ($iColourID < count($aColours)) {
+			$aUserMaps[$sName] = $aColours[$iColourID];
+			$iColourID++;
+		}
+		else {
+			$aUserMaps[$sName] = "000000";
+		}
+	}
+	
+	$sOutput = "|-\n";
+	$sOutput .=  '|<span style="colour: #999999">' . $sTime . '</span>||<span style="color: ' . $aUserMaps[$sName] . '">' . $sName . '</span>||' . $sMinute . "\n";
+	
+	return $sOutput;
 }
 
 ?>
