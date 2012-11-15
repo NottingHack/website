d23883f8fca781d00d50212b1784cf3554b1fb14
James Hayward <jhayward1980@gmail.com>
2012-11-15 22:40:43 +0000

    Adding board meeting processor

---
+++ public_html/tools/bm/process.php
@@ -0,0 +1,33 @@
+<?php
+
+$sFile = "minutes_2012-11-07.txt";
+
+$aMinutes = file($sFile, FILE_IGNORE_NEW_LINES);
+
+$aIgnoreNames = "nh-holly";
+
+foreach ($aMinutes as $sMinute) {
+	if (strpos($sMinute, '[') === 0) {
+		list($sTime, $sMinute) = explode("]", $sMinute);
+		$sTime = trim($sTime, "[");
+		if (strpos($sMinute, ":") !== FALSE) {
+			// something is said
+			list($sName, $sMinute) = explode(":", $sMinute, 2);
+		}
+		else {
+			// Room notification
+			$sName = "";
+		}
+		
+		if (!in_array($sName, $aIgnoreName)) {
+			outputMinute($sTime, $sName, $sMinute);
+		}
+	}
+}
+
+
+function outputMinute($sTime, $sName, $sMinute) {
+	echo($sTime . " > " . $sName . " > " . $sMinute . "<br />\n");
+}
+
+?>
