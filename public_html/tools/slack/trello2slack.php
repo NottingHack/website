d08c9dfd1bb599cfbe441b863d61a2894f6603ae
James Haywward <jhayward1980@gmail.com>
2015-05-30 00:04:26 +0100

    Testing without sending to slack yet

--- public_html/tools/slack/trello2slack.php
+++ public_html/tools/slack/trello2slack.php
@@ -13,26 +13,85 @@ require_once('keys.php');
 // trello details
 $trelloBoardId = '54745ca526448f2011c10a53';
 
+// Date period to look up to for due dates (in days)
+$dueRange = 7;
+
+// Lists to ignore
+$ignoreLists = array("Done");
+
 // get all the members on the board
 
 $url = 'https://api.trello.com/1/boards/' . $trelloBoardId . '/members?key=' . $trelloAppKey . '&token=' . $trelloToken;
 $trelloMembers = json_decode(file_get_contents($url), true);
 
 foreach ($trelloMembers as $member) {
-	$id = searchArray($member['username'], 'trello', $usernames);
+	$i = searchArray($member['username'], 'trello', $usernames);
 	if (!is_null($id)) {
-		$usernames[$id]['trello_id'] = $member['id'];
+		$usernames[$i]['trelloId'] = $member['id'];
 	}
 }
 
 // get all cards on the board
-//https://api.trello.com/1/boards/54745ca526448f2011c10a53/cards?fields=name,idList,url,due,idMembers&key=c90a2dceca7b8daf800215bd8a1584e6&token=54e9d7b257f5112013c0b2007080fb7aa50d9403597c541336e81b2de1b7c74a
-
-//$url = 'https://api.trello.com/1/boards/' . $trelloBoardId . '/cards?fields=name,idList,url,due,idMembers&key=' . $trelloAppKey . '&token=' . $trelloToken;
 $url = 'https://api.trello.com/1/boards/' . $trelloBoardId . '/lists?cards=open&card_fields=name,idList,url,due,idMembers&key=' . $trelloAppKey . '&token=' . $trelloToken;
-$trelloCards = json_decode(file_get_contents($url), true);
+$trelloLists = json_decode(file_get_contents($url), true);
 
-var_dump($trelloCards);
+/*  Let's go through the cards
+	- send a slack message for cards with due dates that are soon, mentioning the user if assigned
+	- send a slack message for assigned cards without dates
+	- count up the unassigned cards without dates on each list
+	- Finally send messages (one per list) of unassigned card count
+*/
+
+foreach ($trelloLists as $list) {
+	if (in_array($list['name'], $ignoreLists)) {
+		continue;
+	}
+
+	$unassigned = 0;
+	foreach ($list['cards'] as $card) {
+		$slackUsers = array();
+		if (count($card['idMembers']) > 0) {
+			foreach ($card['idMembers'] as $trelloId) {
+				$i = searchArray($trelloId, 'trelloId', $usernames);
+				$slackUsers[] = $usernames[$i]['slack'];
+			}
+		}
+
+		if (!is_null($card['due'])) {
+			$days = daysToDue($card['due']);
+
+			if ($days <= $dueRange) {
+				if (count($slackUsers) > 0) {
+					$message = 'Hey ' . implodeSlackUsers($slackUsers) . ' your task "' . $card['name'] . '" ' . dueText($days);
+				}
+				else {
+					$message = 'The task "' . $card['name'] . '" ' . dueText($days) . ' and no one is assigned!';
+				}
+			}
+		}
+		else {
+			if (count($slackUsers) > 0) {
+				$message = 'Hey ' . implodeSlackUsers($slackUsers) . ' your task "' . $card['name'] . " doesn't have a due date";
+			}
+			else {
+				$unassigned++;
+			}
+		}
+		sendSlack($message);
+	}
+
+	if ($unassigned > 0) {
+		$message = 'There ';
+		if ($unassigned == 1) {
+			$message .= "is 1 card that isn't";
+		}
+		else {
+			$message .= 'are ' . $unassigned . " cards that aren't";
+		}
+		$message .= ' assigned to anyone in the "' . $list['name'] . '" list';
+	}
+	sendSlack($message);
+}
 
 
 
@@ -49,4 +108,42 @@ function searchArray($searchValue, $searchKey, $haystack) {
 	}
 	return null;
 }
+
+function sendSlack($message) {
+	echo('<p>' . $message . '</p>');
+}
+
+function implodeSlackUsers($slackUsers) {
+	$userString = "";
+	if (count($slackUsers) > 0) {
+		for ($i = 0; $i < count($slackUsers); $i++) {
+			$userString .= "@" . $slackUsers[$i];
+			if ($i < (count($slackUsers) - 1)) {
+				$userString .= ", ";
+			}
+		}
+	}
+	return $userString;
+}
+
+function dueText($days) {
+	if ($days == 0) {
+		$string = "is due today!";
+	}
+	elseif ($days == 1) {
+		$string = "is due tomorrow";
+	}
+	elseif ($days > 1) {
+		$string = "is due in " . $days . " days";
+	}
+	elseif ($days == -1) {
+		$string = "was due yesterday!";
+	}
+	elseif ($days < -1) {
+		$string = "was due " . $days . " days ago";
+	}
+
+	return $string;
+}
+
 ?>
