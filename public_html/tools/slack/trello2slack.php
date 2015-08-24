8f34eef9fa01f78d4bea8d2c14bd8dc92c8ef0d8
James Hayward <jhayward1980@gmail.com>
2015-08-24 15:15:13 +0100

    Bug hunting

--- public_html/tools/slack/trello2slack.php
+++ public_html/tools/slack/trello2slack.php
@@ -118,6 +118,10 @@ function searchArray($searchValue, $searchKey, $haystack) {
 }
 
 function sendSlack($message) {
+	// testing
+	return;
+
+
 	if ($message == "") {
 		return;
 	}
@@ -149,6 +153,7 @@ function implodeSlackUsers($slackUsers) {
 }
 
 function daysToDue($date) {
+	var_dump($date);
 	list($date, $time) = explode("T", $date);
 	$date = DateTime::createFromFormat('Y-m-d', $date);
 
@@ -160,7 +165,7 @@ function daysToDue($date) {
 	if ($date < $today) {
 		$days = $days * -1;
 	}
-
+	var_dump($days);
 	return $days;
 }
 
